2551e25b46371cada9445a085801187ac2337161
KanadeSup <kanadesupreme@gmail.com>
2025-07-11 14:31:22 +0700

    init commit

---
+++ new-tab/index.tsx
@@ -0,0 +1,21 @@
+import { createRoot } from "react-dom/client";
+import { routeTree } from "./routeTree.gen";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+   interface Register {
+      router: typeof router;
+   }
+}
+
+function init() {
+   const rootContainer = document.querySelector("#__root");
+   if (!rootContainer) throw new Error("Can't find Newtab root element");
+   const root = createRoot(rootContainer);
+   root.render(<RouterProvider router={router} />);
+}
+
+init();
