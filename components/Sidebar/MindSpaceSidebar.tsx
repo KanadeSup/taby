85a934a755ae1957b2f3fc8bb87964803bfcb48b
KanadeSup <kanadesupreme@gmail.com>
2025-07-19 20:16:05 +0700

    feat: implement midnspace item

--- components/Sidebar/MindSpaceSidebar.tsx
+++ components/Sidebar/MindSpaceSidebar.tsx
@@ -25,10 +25,32 @@ function Header() {
 
 function MindSpaceList() {
    return (
-      <div>
-         <div className="flex items-center justify-between p-2">
-            <h1 className="font-bold text-gray-300">Mindspaces</h1>
-            <Plus className="w-7 h-7 text-gray-300 cursor-pointer hover:bg-accent p-1 rounded-sm" />
+      <div className="py-2 px-1 space-y-1">
+         {/* Header */}
+         <div className="flex items-center justify-between">
+            <h1 className="font-bold text-xs px-2 text-gray-300">MINDSPACES</h1>
+            <Plus className="w-6 h-6 text-gray-300 cursor-pointer hover:bg-accent p-1 rounded-sm" />
+         </div>
+         {/* List item */}
+         <div className="flex flex-col gap-1">
+            <MindspaceItem title="Mindspace 1" />
+            <MindspaceItem title="Mindspace 2" />
+            <MindspaceItem title="Mindspace 3" />
+         </div>
+      </div>
+   );
+}
+
+export type MindspaceItemProps = {
+   title: string;
+};
+
+function MindspaceItem(props: MindspaceItemProps) {
+   const { title } = props;
+   return (
+      <div className="flex items-center justify-between px-2 py-1 cursor-pointer hover:bg-accent rounded-sm transition-all">
+         <div className="flex flex-col">
+            <h1 className="font-bold text-gray-300 text-sm">{title}</h1>
          </div>
       </div>
    );
