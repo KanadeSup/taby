0a08db0fa739cf8544d69393cd1f9cadc131394e
NhatNg <nhatnvm@d-soft.com.vn>
2026-04-21 17:37:53 +0700

    feat: implement resizable function for nodes

--- components/FlowNode/CategoryNode.tsx
+++ components/FlowNode/CategoryNode.tsx
@@ -1,26 +1,45 @@
-import { Handle, NodeProps, Position } from "@xyflow/react";
+import { cn } from "@/lib/shadnc-utils";
+import { Handle, NodeProps, NodeResizer, Position } from "@xyflow/react";
 
 export function CategoryNode(props: NodeProps) {
-   const { name, baseNodeSide } = props.data as { name: string; baseNodeSide: "left" | "right" };
+   const { name, baseNodeSide } = props.data as {
+      name: string;
+      baseNodeSide: "left" | "right";
+   };
+   const { selected } = props;
+
    return (
-      <div className="flex items-center gap-2">
-         <Handle
-            id="left"
-            type={baseNodeSide === "left" ? "target" : "source"}
-            position={Position.Left}
-            className="invisible"
+      <>
+         <NodeResizer
+            color="#ff0071"
+            isVisible={selected}
+            minWidth={100}
+            minHeight={30}
          />
-         <div className="rounded-sm p-3 border border-transparent hover:border-accent">
-            <div className="flex items-center gap-1 text-gray-200">
-               <h1 className="text-sm font-semibold truncate">{name}</h1>
+         <div
+            className={cn(
+               "flex items-center gap-2 border-2 border-transparent rounded-sm",
+               "w-full h-full"
+            )}
+         >
+            <Handle
+               id="left"
+               type={baseNodeSide === "left" ? "target" : "source"}
+               position={Position.Left}
+               className="invisible"
+            />
+            <div className="rounded-sm p-3 border border-transparent hover:border-accent w-full h-full">
+               <div className="flex items-center gap-1 text-gray-200">
+                  <h1 className="text-sm font-semibold truncate">{name}</h1>
+               </div>
             </div>
+            <Handle
+               id="right"
+               type={baseNodeSide === "left" ? "source" : "target"}
+               position={Position.Right}
+               className="invisible"
+            />
          </div>
-         <Handle
-            id="right"
-            type={baseNodeSide === "left" ? "source" : "target"}
-            position={Position.Right}
-            className="invisible"
-         />
-      </div>
+      </>
    );
 }
--- components/FlowNode/RootNode.tsx
+++ components/FlowNode/RootNode.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/shadnc-utils";
-import { Handle, NodeProps, Position } from "@xyflow/react";
+import { Handle, NodeProps, NodeResizer, Position } from "@xyflow/react";
 import { Plus } from "lucide-react";
 import { useMindFlowStateStore } from "../Provider/MindFlowStateProvider";
 
@@ -24,45 +24,52 @@ export function RootNode(props: NodeProps) {
       });
    };
    return (
-      <div
-         className={cn(
-            "flex items-center  bg-blue-700 p-4 rounded-md border-2 border-black/50",
-            selected && "border-red-500"
-         )}
-      >
-         <Handle
-            id="left"
-            type="source"
-            position={Position.Left}
-            className="border-0! invisible"
+      <>
+         <NodeResizer
+            color="#ff0071"
+            isVisible={selected}
+            minWidth={100}
+            minHeight={30}
          />
          <div
             className={cn(
-               "hidden absolute left-[-25px] rounded-full bg-green-500 hover:bg-green-600 p-0.5 cursor-pointer",
-               selected && "block"
+               "flex items-center  bg-blue-700 p-4 rounded-md border-2 border-black/50 w-full h-full"
             )}
-            onClick={(e) => handleOpenNodeMenuForInsert(e, "right")}
          >
-            <Plus className="w-3 h-3" />
+            <Handle
+               id="left"
+               type="source"
+               position={Position.Left}
+               className="border-0! invisible"
+            />
+            <div
+               className={cn(
+                  "hidden absolute left-[-25px] rounded-full bg-green-500 hover:bg-green-600 p-0.5 cursor-pointer",
+                  selected && "block"
+               )}
+               onClick={(e) => handleOpenNodeMenuForInsert(e, "right")}
+            >
+               <Plus className="w-3 h-3" />
+            </div>
+            <div className="text-gray-300">
+               <div>Tab root</div>
+            </div>
+            <div
+               className={cn(
+                  "hidden absolute right-[-25px] rounded-full bg-green-500 hover:bg-green-600 p-0.5 cursor-pointer",
+                  selected && "block"
+               )}
+               onClick={(e) => handleOpenNodeMenuForInsert(e, "left")}
+            >
+               <Plus className="w-3 h-3" />
+            </div>
+            <Handle
+               id="right"
+               type="source"
+               position={Position.Right}
+               className="border-0! invisible"
+            />
          </div>
-         <div className="text-gray-300">
-            <div>Tab root</div>
-         </div>
-         <div
-            className={cn(
-               "hidden absolute right-[-25px] rounded-full bg-green-500 hover:bg-green-600 p-0.5 cursor-pointer",
-               selected && "block"
-            )}
-            onClick={(e) => handleOpenNodeMenuForInsert(e, "left")}
-         >
-            <Plus className="w-3 h-3" />
-         </div>
-         <Handle
-            id="right"
-            type="source"
-            position={Position.Right}
-            className="border-0! invisible"
-         />
-      </div>
+      </>
    );
 }
